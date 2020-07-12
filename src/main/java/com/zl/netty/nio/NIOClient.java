1cb027872feee15cade6c9e19606f723dcd43b01
zhanglan <474529814@qq.com>
2020-07-12 13:09:34 +0800

    nio代码

---
+++ src/main/java/com/zl/netty/nio/NIOClient.java
@@ -0,0 +1,75 @@
+package com.zl.netty.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * @author zhang_lan@inspur.com
+ * @description
+ * @date 2020/7/12 0012
+ */
+public class NIOClient {
+
+    private Selector selector;
+
+    public void initClient(String ip, int port) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        this.selector = Selector.open();
+        socketChannel.connect(new InetSocketAddress(ip, port));
+        socketChannel.register(selector, SelectionKey.OP_CONNECT);
+    }
+
+    public void connect() throws IOException {
+        while (true) {
+            selector.select();
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                iterator.remove();
+                if (selectionKey.isConnectable()) {
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                    if (socketChannel.isConnectionPending()) {
+                        socketChannel.finishConnect();
+                    }
+                    socketChannel.configureBlocking(false);
+                    ByteBuffer buffer = ByteBuffer.wrap("hello server".getBytes());
+                    socketChannel.write(buffer);
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                } else if (selectionKey.isReadable()) {
+                    read(selectionKey);
+                } else if (selectionKey.isWritable()) {
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                }
+            }
+        }
+    }
+
+    public void read(SelectionKey key) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int len = socketChannel.read(buffer);
+        if (len != -1) {
+            System.out.println("客户端收到的信息：" + new String(buffer.array(), 0, len));
+        }
+        Scanner scanner = new Scanner(System.in);
+        String msg = scanner.next();
+        ByteBuffer writeBuffer = ByteBuffer.wrap(msg.getBytes());
+        socketChannel.write(writeBuffer);
+        socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+    }
+
+    public static void main(String[] args) throws IOException {
+        NIOClient client = new NIOClient();
+        client.initClient("127.0.0.1", 9000);
+        client.connect();
+    }
+
+}
--- src/main/java/com/zl/netty/nio/NIOServer.java
+++ src/main/java/com/zl/netty/nio/NIOServer.java
@@ -1,10 +1,13 @@
 package com.zl.netty.nio;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
 /**
@@ -13,23 +16,63 @@ import java.util.Iterator;
  * @date 2020/7/9
  */
 public class NIOServer {
-	public static void main(String[] args) throws IOException {
-		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-		serverSocketChannel.configureBlocking(false);
-		serverSocketChannel.bind(new InetSocketAddress(9000));
-		Selector selector = Selector.open();
-		serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-		while (true) {
-			System.out.println("等待事件...");
-			int select = selector.select();
-			Iterator<SelectionKey> it = selector.selectedKeys().iterator();
-			while (it.hasNext()) {
-				SelectionKey selectionKey = it.next();
-				it.remove();
+	private Selector selector;
 
+	private ServerSocketChannel serverChannel;
+
+	public void initServer() {
+		try {
+			this.serverChannel = ServerSocketChannel.open();
+			this.selector = Selector.open();
+			this.serverChannel.configureBlocking(false);
+			this.serverChannel.bind(new InetSocketAddress(9000));
+			this.serverChannel.register(this.selector, SelectionKey.OP_ACCEPT);
+			while (true) {
+				this.selector.select();
+				Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+				while (it.hasNext()) {
+					SelectionKey key = it.next();
+					it.remove();
+					handle(key);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				this.serverChannel.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
-
 	}
+
+	private void handle(SelectionKey key) throws IOException {
+		if (key.isAcceptable()) {
+			ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+			SocketChannel socketChannel = serverSocketChannel.accept();
+			socketChannel.configureBlocking(false);
+			socketChannel.register(this.selector, SelectionKey.OP_READ);
+		} else if (key.isReadable()) {
+			SocketChannel socketChannel = (SocketChannel) key.channel();
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			int len = socketChannel.read(buffer);
+			if (len != -1) {
+				System.out.println("读取到客户端发送来的数据：" + new String(buffer.array(), 0, len));
+			}
+			ByteBuffer writeBuffer = ByteBuffer.wrap("hello client".getBytes());
+			socketChannel.write(writeBuffer);
+			key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+		} else if (key.isWritable()) {
+			SocketChannel socketChannel = (SocketChannel) key.channel();
+			key.interestOps(SelectionKey.OP_READ);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		NIOServer server = new NIOServer();
+		server.initServer();
+	}
+
 }
